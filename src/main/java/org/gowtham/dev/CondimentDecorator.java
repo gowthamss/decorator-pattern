706308f3a75218e70e90a7a49f37c9270caa3eed
gowthamss <gowthambudida010@gmail.com>
2024-12-16 18:05:17 +0530

    feat: add size and compute cost

--- src/main/java/org/gowtham/dev/CondimentDecorator.java
+++ src/main/java/org/gowtham/dev/CondimentDecorator.java
@@ -5,4 +5,9 @@ public abstract class CondimentDecorator extends Beverage
     protected Beverage beverage;
 
     public abstract String getDescription();
+
+    @Override public Size getSize()
+    {
+        return beverage.getSize();
+    }
 }
